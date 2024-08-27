d31b75dd542c92d26bce0ca12c219bcbb534ff23
empty <50263213+ShaharNaveh@users.noreply.github.com>
2024-08-27 10:51:16 +0300

    Save

--- content/blog/practical-networking-guide/scripts/generate_cidrs_table.py
+++ content/blog/practical-networking-guide/scripts/generate_cidrs_table.py
@@ -1,14 +1,31 @@
 #!/usr/bin/env python
-import csv
+import contextlib
 import ipaddress
 import pathlib
+from typing import TYPE_CHECKING
 
-OUTPUT_FILE = pathlib.Path(__file__).parents[1] / "assets" / "cidrs.csv"
+if TYPE_CHECKING:
+    from collections.abc import Iterator
+
 BASE_IP = "0.0.0.0"
+SEP = " | "
+
+OUTPUT_FILE = pathlib.Path(__file__).parents[1] / "assets" / "cidrs.md"
 CIDRS = range(ipaddress.IPV4LENGTH + 1)
 
 
-def ip_cidr_info(cidr: int) -> dict[str, str | int]:
+@contextlib.contextmanager
+def write_ro_file(path: pathlib.Path) -> "Iterator[pathlib.Path]":
+    try:
+      path.chmod(0o644)  # Allow write
+    except FileNotFoundError:
+      # We don't care if file doesn't exists yet
+      pass
+    yield path
+    path.chmod(0o444)  # Set to readonly
+
+
+def ip_cidr_info(cidr: int) -> dict[str, str]:
     ip_network = ipaddress.ip_network(f"{BASE_IP}/{cidr}")
 
     network_address = ip_network.network_address
@@ -26,23 +43,33 @@ def ip_cidr_info(cidr: int) -> dict[str, str | int]:
 
     res = {}
     res["CIDR"] = f"/{cidr}"
-    res["Network Address"] = str(network_address)
+    res["Network Address"] = network_address
     res["Usable Host IP Range"] = usable_host_ip_range
-    res["Broadcast Address"] = str(broadcast_address)
-    res["Netmask"] = str(ip_network.netmask)
-    res["Total Number of Hosts"] = f"{num_addresses:3,}".strip()
-    res["Number of Usable Hosts"] = f"{number_of_usable_hosts:3,}".strip()
-    return res
+    res["Broadcast Address"] = broadcast_address
+    res["Netmask"] = ip_network.netmask
+    res["Total Number of Hosts"] = f"{num_addresses:3,}"
+    res["Number of Usable Hosts"] = f"{number_of_usable_hosts:3,}"
 
-fieldnames = ip_cidr_info(ipaddress.IPV4LENGTH).keys()
+    info = {k.strip(): str(v).strip() for k, v in res.items()}
+    return info
 
-OUTPUT_FILE.chmod(0o644) # Allow write
 
-with OUTPUT_FILE.open(mode="w", newline="") as csvfile:
-    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-    writer.writeheader()
-    for cidr in CIDRS:
-        row = ip_cidr_info(cidr)
-        writer.writerow(row)
 
-OUTPUT_FILE.chmod(0o444) # Set to readonly
+def cidr_info_md_rows():
+  for cidr in CIDRS:
+    cidr_info = ip_cidr_info(cidr)
+    row = "{sep}{data}{sep}".format(sep=SEP, data = SEP.join(cidr_info.values())).strip()
+    yield row
+
+
+headers = ip_cidr_info(ipaddress.IPV4LENGTH).keys()
+table_headers = "{sep}{headers}{sep}".format(sep=SEP, headers=SEP.join(headers)).strip()
+table_sep = "{sep}{seperator}".format(
+    sep=SEP, seperator=f"---{SEP}" * len(headers)
+).strip()
+table_rows = "\n".join(cidr_info_md_rows())
+
+table = "\n".join([table_headers, table_sep, table_rows])
+
+with write_ro_file(OUTPUT_FILE) as path:
+  path.write_text(table)
